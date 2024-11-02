b674c2e4b8bdcf3f7adf955b38355a81dc02bcd7
jutudey <aj@eriksen.cz>
2024-11-02 18:43:52 +0100

    removed locale completely

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
 import datetime
-import locale
 import pandas as pd
 import streamlit as st
 from PIL import Image
@@ -97,10 +96,6 @@ def set_page_definitition():
   st.set_page_config(layout="wide", page_title=app_name, page_icon=icon, initial_sidebar_state="expanded")
   # st.set_page_config(layout="wide", page_title=app_name, page_icon=":material/sound_detection_dog_barking:", initial_sidebar_state="expanded")
 
-
-
-  # Set locale to use appropriate number format
-  locale.setlocale(locale.LC_ALL, 'en_GB.UTF-8')  # Use 'en_GB' for UK locale
 
   return app_name
 
