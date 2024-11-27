f4a3345794038afb27491689922c0874b7ce3bd0
jutudey <aj@eriksen.cz>
2024-11-27 17:27:55 +0100

    removed the spare tab on the inspect data page

---
+++ pages/EDA.py
@@ -0,0 +1,11 @@
+import config
+import streamlit as st
+import pandas as pd
+import datetime
+import functions
+
+functions.set_page_definitition()
+st.title("EDA - Exploratory Data Analysis")
+
+
+
--- pages/Inspect Data.py
+++ pages/Inspect Data.py
@@ -11,7 +11,7 @@ with st.spinner('Loading and preparing data...'):
     functions.initialize_session_state()
 
 # define 2 streamlit tabs
-raw_data_tab, invoice_lines_tab = st.tabs(["Inspect the source files", "Inspect the processed Invoice Lines Data"])
+# raw_data_tab, invoice_lines_tab = st.tabs(["Inspect the source files", "Inspect the processed Invoice Lines Data"])
 
 # with raw_data_tab:
 #     files_for_analysis = [[sublist[0], sublist[2]] for sublist in config.required_files_description]
@@ -51,44 +51,44 @@ raw_data_tab, invoice_lines_tab = st.tabs(["Inspect the source files", "Inspect 
 #         else:
 #             st.error("The file format is not supported.")
 
-with invoice_lines_tab:
-    df = st.session_state.all_invoice_lines
+# with invoice_lines_tab:
+df = st.session_state.all_invoice_lines
 
-    # if df is None:
-    #     df = functions.merge_invoice_lines_and_payments()
-    #     st.session_state['df'] = df
+# if df is None:
+#     df = functions.merge_invoice_lines_and_payments()
+#     st.session_state['df'] = df
 
-    st.title("️🔍  Inspect the Invoice Lines Data")
+st.title("️🔍  Inspect the Invoice Lines Data")
 
-    filter_col, empty_col, summary_col = st.columns([5, 0.5, 2])
+filter_col, empty_col, summary_col = st.columns([5, 0.5, 2])
 
-    with filter_col:
-        filtered_df = functions.filter_dataframe(df)
+with filter_col:
+    filtered_df = functions.filter_dataframe(df)
 
-        number_invoice_lines = filtered_df["Invoice Line ID"].count()
-        sum_internal_cost = f"£{filtered_df['Product Cost'].sum():,.2f}"
-        sum_sales_price = f"£{filtered_df['Standard Price(incl)'].sum():,.2f}"
-        theoretical_profit_value = filtered_df['Standard Price(incl)'].sum() - filtered_df["Product Cost"].sum()
-        theoretical_profit = f"£{theoretical_profit_value:,.2f}"
-        sum_actual_invoiced = f"£{filtered_df['Total Invoiced (incl)'].sum():,.2f}"
-        applied_discount_value = filtered_df['Standard Price(incl)'].sum() - filtered_df['Total Invoiced (incl)'].sum()
-        applied_discount = f"£{applied_discount_value:,.2f}"
+    number_invoice_lines = filtered_df["Invoice Line ID"].count()
+    sum_internal_cost = f"£{filtered_df['Product Cost'].sum():,.2f}"
+    sum_sales_price = f"£{filtered_df['Standard Price(incl)'].sum():,.2f}"
+    theoretical_profit_value = filtered_df['Standard Price(incl)'].sum() - filtered_df["Product Cost"].sum()
+    theoretical_profit = f"£{theoretical_profit_value:,.2f}"
+    sum_actual_invoiced = f"£{filtered_df['Total Invoiced (incl)'].sum():,.2f}"
+    applied_discount_value = filtered_df['Standard Price(incl)'].sum() - filtered_df['Total Invoiced (incl)'].sum()
+    applied_discount = f"£{applied_discount_value:,.2f}"
 
 
-        st.dataframe(filtered_df[[
-            'Invoice Line Date: Created', 'Invoice Line ID',
-            'Invoice Date', 'Product Name', 'Product Description', 'Product Group', 'reporting_categories',
-            'Created By', 'created_by_category', 'Client Contact Code',
-            'Business Name', 'First Name', 'Last Name',
-            'Animal Code', 'Animal Name', 'Species', 'Breed',
-            'Product Cost', 'Standard Price(incl)', 'Discount(£)', 'Total Invoiced (incl)'
-        ]], hide_index=True)
+    st.dataframe(filtered_df[[
+        'Invoice Line Date: Created', 'Invoice Line ID',
+        'Invoice Date', 'Product Name', 'Product Description', 'Product Group', 'reporting_categories',
+        'Created By', 'created_by_category', 'Client Contact Code',
+        'Business Name', 'First Name', 'Last Name',
+        'Animal Code', 'Animal Name', 'Species', 'Breed',
+        'Product Cost', 'Standard Price(incl)', 'Discount(£)', 'Total Invoiced (incl)'
+    ]], hide_index=True)
 
-    with summary_col:
-        container = st.container(border=True)
-        container.write(f"Number of Invoice Lines: {number_invoice_lines}")
-        container.write(f"Sum Internal Cost: {sum_internal_cost}")
-        container.write(f"Sum Sales Price: {sum_sales_price}")
-        container.write(f"Theoretical Profit: {theoretical_profit}")
-        container.write(f"Sum Actual Invoiced: {sum_actual_invoiced}")
-        container.write(f"Applied Discounts: {applied_discount}")
+with summary_col:
+    container = st.container(border=True)
+    container.write(f"Number of Invoice Lines: {number_invoice_lines}")
+    container.write(f"Sum Internal Cost: {sum_internal_cost}")
+    container.write(f"Sum Sales Price: {sum_sales_price}")
+    container.write(f"Theoretical Profit: {theoretical_profit}")
+    container.write(f"Sum Actual Invoiced: {sum_actual_invoiced}")
+    container.write(f"Applied Discounts: {applied_discount}")
