563a2df2e4bd45fc1bb63db4afd7bd700c860e6e
jutudey <aj@eriksen.cz>
2024-11-28 18:54:46 +0100

    added a new page

---
+++ pages/ezyVet Disipline.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import datetime
+import altair as alt
+import functions
+
+# General page setup
+functions.set_page_definitition()
+
+functions.initialize_session_state()
+
+st.title("ezyVet Disipline")
